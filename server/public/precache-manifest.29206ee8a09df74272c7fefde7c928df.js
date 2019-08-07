e71744612e092147c13b62972f76b18a026329ac
Sergio Velikopoljski <sergiovelo17@gmail.com>
2019-08-07 15:26:03 -0400

    build

---
+++ server/public/precache-manifest.29206ee8a09df74272c7fefde7c928df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f7beb5dd0300dc82d41ada853b54f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a948045fa34ca11314",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "f14db1bbf300a3d99fb9",
+    "url": "/static/css/main.b869d192.chunk.css"
+  },
+  {
+    "revision": "a5a948045fa34ca11314",
+    "url": "/static/js/2.bce35815.chunk.js"
+  },
+  {
+    "revision": "f14db1bbf300a3d99fb9",
+    "url": "/static/js/main.20c367a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
